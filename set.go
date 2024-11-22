ce0730facb9e5ebfb8bb4b677b8dd791c7252259
Yurii Biurher <yurii.biurher@zorallabs.com>
2024-11-22 18:07:02 +0200

    refactoring

--- set.go
+++ set.go
@@ -10,9 +10,9 @@ import (
 type Set[T comparable] map[T]interface{}
 
 // NewSet creates a new set. The set can be initialized with other sets.
-func NewSet[T comparable](src ...Set[T]) Set[T] {
+func NewSet[T comparable](sources ...Set[T]) Set[T] {
 	set := make(Set[T])
-	for _, s := range src {
+	for _, s := range sources {
 		set.AddSet(s)
 	}
 	return set
@@ -36,21 +36,18 @@ func NewSetFromIter[T comparable](is iter.Seq[T]) Set[T] {
 	return set
 }
 
-// Len returns the number of elements in the set.
-func (s Set[T]) Len() int {
-	return len(s)
+// NewSetFromArgs creates a new set from arguments.
+func NewSetFromArgs[T comparable](es ...T) Set[T] {
+	set := make(Set[T])
+	for _, e := range es {
+		set[e] = nil
+	}
+	return set
 }
 
 // Add adds elements to the set.
 func (s Set[T]) Add(es ...T) {
 	for _, e := range es {
-		s[e] = nil
-	}
-}
-
-// AddIter adds elements to the set from an iterator.
-func (s Set[T]) AddIter(is iter.Seq[T]) {
-	for e := range is {
 		s[e] = nil
 	}
 }
@@ -69,9 +66,37 @@ func (s Set[T]) AddSlice(es []T) {
 	}
 }
 
+// AddIter adds elements to the set from an iterator.
+func (s Set[T]) AddIter(is iter.Seq[T]) {
+	for e := range is {
+		s[e] = nil
+	}
+}
+
 // Remove removes elements from the set.
 func (s Set[T]) Remove(es ...T) {
 	for _, e := range es {
+		delete(s, e)
+	}
+}
+
+// RemoveSet removes elements from the set present in another set.
+func (s Set[T]) RemoveSet(other Set[T]) {
+	for e := range other {
+		delete(s, e)
+	}
+}
+
+// RemoveSlice removes elements from the set present in a slice.
+func (s Set[T]) RemoveSlice(es []T) {
+	for _, e := range es {
+		delete(s, e)
+	}
+}
+
+// RemoveIter removes elements from the set present in a sequence.
+func (s Set[T]) RemoveIter(is iter.Seq[T]) {
+	for e := range is {
 		delete(s, e)
 	}
 }
@@ -92,6 +117,36 @@ func (s Set[T]) ContainsAll(es ...T) bool {
 	return true
 }
 
+// ContainsAny returns true if the set contains all elements of other set.
+func (s Set[T]) ContainsAllFromSet(other Set[T]) bool {
+	for e := range other {
+		if !s.Contains(e) {
+			return false
+		}
+	}
+	return true
+}
+
+// ContainsAny returns true if the set contains all elements of slice.
+func (s Set[T]) ContainsAllFromSlice(es []T) bool {
+	for _, e := range es {
+		if !s.Contains(e) {
+			return false
+		}
+	}
+	return true
+}
+
+// ContainsAny returns true if the set contains all elements of sequence.
+func (s Set[T]) ContainsAllFromIter(is iter.Seq[T]) bool {
+	for e := range is {
+		if !s.Contains(e) {
+			return false
+		}
+	}
+	return true
+}
+
 // ContainsAny returns true if the set contains any of the elements.
 func (s Set[T]) ContainsAny(es ...T) bool {
 	for _, e := range es {
@@ -102,6 +157,59 @@ func (s Set[T]) ContainsAny(es ...T) bool {
 	return false
 }
 
+// ContainsAnyFromSet returns true if the set contains any of the elements of other set.
+func (s Set[T]) ContainsAnyFromSet(other Set[T]) bool {
+	for e := range other {
+		if s.Contains(e) {
+			return true
+		}
+	}
+	return false
+}
+
+// ContainsAnyFromSlice returns true if the set contains any of the elements of slice.
+func (s Set[T]) ContainsAnyFromSlice(es []T) bool {
+	for _, e := range es {
+		if s.Contains(e) {
+			return true
+		}
+	}
+	return false
+}
+
+// ContainsAnyFromIter returns true if the set contains any of the elements of sequence.
+func (s Set[T]) ContainsAnyFromIter(is iter.Seq[T]) bool {
+	for e := range is {
+		if s.Contains(e) {
+			return true
+		}
+	}
+	return false
+}
+
+// Len returns the number of elements in the set.
+func (s Set[T]) Len() int {
+	return len(s)
+}
+
+// Clear removes all elements from the set.
+func (s Set[T]) Clear() {
+	clear(s)
+}
+
+// Clone returns a copy of the set.
+func (s Set[T]) Clone() Set[T] {
+	return NewSet(s)
+}
+
+// Equal returns true if the set is equal to another set.
+func (s Set[T]) Equal(other Set[T]) bool {
+	if len(s) != len(other) {
+		return false
+	}
+	return s.ContainsAllFromSet(other)
+}
+
 // Slice returns the elements of the set as a slice.
 func (s Set[T]) Slice() []T {
 	return slices.Collect(maps.Keys(s))
@@ -110,4 +218,50 @@ func (s Set[T]) Slice() []T {
 // Iter returns an iterator over the elements of the set.
 func (s Set[T]) Iter() iter.Seq[T] {
 	return maps.Keys(s)
+}
+
+// IsEmpty returns true if the set is empty.
+func (s Set[T]) IsEmpty() bool {
+	return len(s) == 0
+}
+
+// IsSubsetOf returns true if the set is a subset of another set.
+func (s Set[T]) IsSubsetOf(other Set[T]) bool {
+	return other.ContainsAllFromSet(s)
+}
+
+// IsSupersetOf returns true if the set is a superset of another set.
+func (s Set[T]) IsSupersetOf(other Set[T]) bool {
+	return s.ContainsAllFromSet(other)
+}
+
+// IsProperSubsetOf returns true if the set is a proper subset of another set.
+func (s Set[T]) IsProperSubsetOf(other Set[T]) bool {
+	return s.IsSubsetOf(other) && !s.Equal(other)
+}
+
+// IsProperSupersetOf returns true if the set is a proper superset of another set.
+func (s Set[T]) IsProperSupersetOf(other Set[T]) bool {
+	return s.IsSupersetOf(other) && !s.Equal(other)
+}
+
+// Diff returns the difference between the set and another set.
+func (s Set[T]) Diff(other Set[T]) Set[T] {
+	diff := NewSet(s)
+	diff.RemoveSet(other)
+	return diff
+}
+
+// SymmetricDiff returns the symmetric difference between the set and another set.
+func (s Set[T]) SymmetricDiff(other Set[T]) Set[T] {
+	t := s.Diff(other)
+	t.AddSet(other.Diff(s))
+	return t
+}
+
+// Union returns the union of the set and another set.
+func (s Set[T]) Union(other Set[T]) Set[T] {
+	union := NewSet(s)
+	union.AddSet(other)
+	return union
 }
--- set_test.go
+++ set_test.go
@@ -1,6 +1,9 @@
 package got
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSet(t *testing.T) {
 	t.Run("Set of ints", func(t *testing.T) {
@@ -58,6 +61,367 @@ func TestSet(t *testing.T) {
 		}
 		if s.Contains(4) {
 			t.Error("Set should not contain 4")
+		}
+	})
+
+	t.Run("Set of ints from other sets", func(t *testing.T) {
+		s1 := NewSetFromArgs(1, 2, 3)
+		s2 := NewSetFromArgs(2, 3, 4)
+		s := NewSet[int](s1, s2)
+		if !s.Contains(1) {
+			t.Error("Set should contain 1")
+		}
+		if !s.Contains(2) {
+			t.Error("Set should contain 2")
+		}
+		if !s.Contains(3) {
+			t.Error("Set should contain 3")
+		}
+		if !s.Contains(4) {
+			t.Error("Set should contain 4")
+		}
+		if s.Contains(5) {
+			t.Error("Set should not contain 5")
+		}
+	})
+
+	t.Run("Set of ints from iterators", func(t *testing.T) {
+		slice := []int{1, 2, 3}
+		s := NewSetFromIter(slices.Values(slice))
+		if !s.Contains(1) {
+			t.Error("Set should contain 1")
+		}
+		if !s.Contains(2) {
+			t.Error("Set should contain 2")
+		}
+		if !s.Contains(3) {
+			t.Error("Set should contain 3")
+		}
+		if s.Contains(4) {
+			t.Error("Set should not contain 4")
+		}
+	})
+
+	t.Run("Set of ints add", func(t *testing.T) {
+		s := NewSet[int]()
+		s.Add(1, 2, 3)
+		if !s.Contains(1) {
+			t.Error("Set should contain 1")
+		}
+		if !s.Contains(2) {
+			t.Error("Set should contain 2")
+		}
+		if !s.Contains(3) {
+			t.Error("Set should contain 3")
+		}
+		if s.Contains(4) {
+			t.Error("Set should not contain 4")
+		}
+
+		s.AddSet(NewSetFromArgs(4, 5, 6))
+		if !s.Contains(4) {
+			t.Error("Set should contain 4")
+		}
+		if !s.Contains(5) {
+			t.Error("Set should contain 5")
+		}
+		if !s.Contains(6) {
+			t.Error("Set should contain 6")
+		}
+
+		s.AddSlice([]int{7, 8, 9})
+		if !s.Contains(7) {
+			t.Error("Set should contain 7")
+		}
+		if !s.Contains(8) {
+			t.Error("Set should contain 8")
+		}
+		if !s.Contains(9) {
+			t.Error("Set should contain 9")
+		}
+
+		slice := []int{10, 11, 12}
+		s.AddIter(slices.Values(slice))
+		if !s.Contains(10) {
+			t.Error("Set should contain 10")
+		}
+		if !s.Contains(11) {
+			t.Error("Set should contain 11")
+		}
+		if !s.Contains(12) {
+			t.Error("Set should contain 12")
+		}
+	})
+
+	t.Run("Set of ints remove", func(t *testing.T) {
+		s := NewSetFromArgs(1, 2, 3, 4, 5, 6, 7, 9)
+		s.Remove(1, 2)
+		if s.Contains(1) {
+			t.Error("Set should not contain 1")
+		}
+		if s.Contains(2) {
+			t.Error("Set should not contain 2")
+		}
+
+		s.RemoveSet(NewSetFromArgs(3, 4))
+		if s.Contains(3) {
+			t.Error("Set should not contain 3")
+		}
+		if s.Contains(4) {
+			t.Error("Set should not contain 4")
+		}
+
+		s.RemoveSlice([]int{5, 6})
+		if s.Contains(5) {
+			t.Error("Set should not contain 5")
+		}
+		if s.Contains(6) {
+			t.Error("Set should not contain 6")
+		}
+
+		slice := []int{7, 9}
+		s.RemoveIter(slices.Values(slice))
+		if s.Contains(7) {
+			t.Error("Set should not contain 7")
+		}
+		if s.Contains(9) {
+			t.Error("Set should not contain 9")
+		}
+	})
+
+	t.Run("Set of ints clear", func(t *testing.T) {
+		s := NewSetFromArgs(1, 2, 3)
+		if s.IsEmpty() {
+			t.Error("Set should not be empty")
+		}
+		s.Clear()
+		if !s.IsEmpty() {
+			t.Error("Set should be empty")
+		}
+	})
+
+	t.Run("Set of ints contains all", func(t *testing.T) {
+		s := NewSetFromArgs(1, 2, 3)
+		if !s.ContainsAll(1, 2) {
+			t.Error("Set should contain 1 and 2")
+		}
+		if s.ContainsAll(1, 4) {
+			t.Error("Set should not contain 4")
+		}
+		if !s.ContainsAllFromSet(NewSetFromArgs(1, 2)) {
+			t.Error("Set should contain 1 and 2")
+		}
+		if s.ContainsAllFromSet(NewSetFromArgs(1, 4)) {
+			t.Error("Set should not contain 4")
+		}
+		if !s.ContainsAllFromSlice([]int{1, 2}) {
+			t.Error("Set should contain 1 and 2")
+		}
+		if s.ContainsAllFromSlice([]int{1, 4}) {
+			t.Error("Set should not contain 4")
+		}
+
+		slice := []int{1, 2}
+		if !s.ContainsAllFromIter(slices.Values(slice)) {
+			t.Error("Set should contain 1 and 2")
+		}
+		slice = []int{1, 4}
+		if s.ContainsAllFromIter(slices.Values(slice)) {
+			t.Error("Set should not contain 4")
+		}
+	})
+
+	t.Run("Set of ints contains any", func(t *testing.T) {
+		s := NewSetFromArgs(1, 2, 3)
+		if !s.ContainsAny(1, 4) {
+			t.Error("Set should contain 1")
+		}
+		if s.ContainsAny(4, 5) {
+			t.Error("Set should not contain 4 or 5")
+		}
+		if !s.ContainsAnyFromSet(NewSetFromArgs(1, 4)) {
+			t.Error("Set should contain 1")
+		}
+		if s.ContainsAnyFromSet(NewSetFromArgs(4, 5)) {
+			t.Error("Set should not contain 4 or 5")
+		}
+		if !s.ContainsAnyFromSlice([]int{1, 4}) {
+			t.Error("Set should contain 1")
+		}
+		if s.ContainsAnyFromSlice([]int{4, 5}) {
+			t.Error("Set should not contain 4 or 5")
+		}
+
+		slice := []int{1, 4}
+		if !s.ContainsAnyFromIter(slices.Values(slice)) {
+			t.Error("Set should contain 1")
+		}
+		slice = []int{4, 5}
+		if s.ContainsAnyFromIter(slices.Values(slice)) {
+			t.Error("Set should not contain 4 or 5")
+		}
+	})
+
+	t.Run("Set of ints len", func(t *testing.T) {
+		s := NewSetFromArgs(1, 2, 3)
+		if s.Len() != 3 {
+			t.Error("Set should have length 3")
+		}
+	})
+
+	t.Run("Set of ints equal", func(t *testing.T) {
+		s1 := NewSetFromArgs(1, 2, 3)
+		s2 := NewSetFromArgs(1, 2, 3)
+		if !s1.Equal(s2) {
+			t.Error("Sets should be equal")
+		}
+		s3 := NewSetFromArgs(1, 2, 3, 4)
+		if s1.Equal(s3) {
+			t.Error("Sets should not be equal")
+		}
+	})
+
+	t.Run("Set of ints clone", func(t *testing.T) {
+		s1 := NewSetFromArgs(1, 2, 3)
+		s2 := s1.Clone()
+		if !s1.Equal(s2) {
+			t.Error("Sets should be equal")
+		}
+	})
+
+	t.Run("Set of ints slice", func(t *testing.T) {
+		s := NewSetFromArgs(1, 2, 3)
+		slice := s.Slice()
+		if len(slice) != 3 {
+			t.Error("Slice should have length 3")
+		}
+		if slice[0] != 1 {
+			t.Error("Slice should contain 1")
+		}
+		if slice[1] != 2 {
+			t.Error("Slice should contain 2")
+		}
+		if slice[2] != 3 {
+			t.Error("Slice should contain 3")
+		}
+	})
+
+	t.Run("Set of ints iter", func(t *testing.T) {
+		s := NewSetFromArgs(1, 2, 3)
+		for e := range s.Iter() {
+			if !s.Contains(e) {
+				t.Errorf("Set should contain %d", e)
+			}
+		}
+	})
+
+	t.Run("Set of ints is subset of", func(t *testing.T) {
+		s1 := NewSetFromArgs(1, 2, 3)
+		s2 := NewSetFromArgs(1, 2, 3, 4)
+		if !s1.IsSubsetOf(s2) {
+			t.Error("Set should be a subset of other set")
+		}
+		if s2.IsSubsetOf(s1) {
+			t.Error("Set should not be a subset of other set")
+		}
+	})
+
+	t.Run("Set of ints is superset of", func(t *testing.T) {
+		s1 := NewSetFromArgs(1, 2, 3)
+		s2 := NewSetFromArgs(1, 2, 3, 4)
+		if s1.IsSupersetOf(s2) {
+			t.Error("Set should not be a superset of other set")
+		}
+		if !s2.IsSupersetOf(s1) {
+			t.Error("Set should be a superset of other set")
+		}
+	})
+
+	t.Run("Set of ints is proper subset of", func(t *testing.T) {
+		s1 := NewSetFromArgs(1, 2, 3)
+		s2 := NewSetFromArgs(1, 2, 3, 4)
+		if !s1.IsProperSubsetOf(s2) {
+			t.Error("Set should be a proper subset of other set")
+		}
+		if s2.IsProperSubsetOf(s1) {
+			t.Error("Set should not be a proper subset of other set")
+		}
+		s3 := NewSetFromArgs(1, 2, 3)
+		if s1.IsProperSubsetOf(s3) {
+			t.Error("Set should not be a proper subset of other set")
+		}
+	})
+
+	t.Run("Set of ints is proper superset of", func(t *testing.T) {
+		s1 := NewSetFromArgs(1, 2, 3)
+		s2 := NewSetFromArgs(1, 2, 3, 4)
+		if s1.IsProperSupersetOf(s2) {
+			t.Error("Set should not be a proper superset of other set")
+		}
+		if !s2.IsProperSupersetOf(s1) {
+			t.Error("Set should be a proper superset of other set")
+		}
+		s3 := NewSetFromArgs(1, 2, 3)
+		if s1.IsProperSupersetOf(s3) {
+			t.Error("Set should not be a proper superset of other set")
+		}
+	})
+
+	t.Run("Set of ints diff", func(t *testing.T) {
+		s1 := NewSetFromArgs(1, 2, 3)
+		s2 := NewSetFromArgs(1, 2, 3, 4)
+		diff1 := s1.Diff(s2)
+		if diff1.Len() != 0 {
+			t.Error("Diff should be empty")
+		}
+		diff2 := s2.Diff(s1)
+		if diff2.Len() != 1 {
+			t.Error("Diff should have length 1")
+		}
+		if !diff2.Contains(4) {
+			t.Error("Diff should contain 4")
+		}
+	})
+
+	t.Run("Set of ints symmetric diff", func(t *testing.T) {
+		s1 := NewSetFromArgs(1, 2, 3, 4, 5)
+		s2 := NewSetFromArgs(2, 4, 6)
+		diff := s1.SymmetricDiff(s2)
+		if diff.Len() != 4 {
+			t.Error("Diff should have length 4")
+		}
+		if !diff.Contains(1) {
+			t.Error("Diff should contain 1")
+		}
+		if !diff.Contains(3) {
+			t.Error("Diff should contain 3")
+		}
+		if !diff.Contains(5) {
+			t.Error("Diff should contain 5")
+		}
+		if !diff.Contains(6) {
+			t.Error("Diff should contain 6")
+		}
+	})
+
+	t.Run("Set of ints union", func(t *testing.T) {
+		s1 := NewSetFromArgs(1, 2, 3)
+		s2 := NewSetFromArgs(2, 3, 4)
+		union := s1.Union(s2)
+		if union.Len() != 4 {
+			t.Error("Union should have length 4")
+		}
+		if !union.Contains(1) {
+			t.Error("Union should contain 1")
+		}
+		if !union.Contains(2) {
+			t.Error("Union should contain 2")
+		}
+		if !union.Contains(3) {
+			t.Error("Union should contain 3")
+		}
+		if !union.Contains(4) {
+			t.Error("Union should contain 4")
 		}
 	})
 }
