c69460c0141f53fd1e978c8a5ce5d70d8718504a
Yurii Biurher <yurii.biurher@zorallabs.com>
2024-11-24 15:25:33 +0200

    refactoring

---
+++ range_test.go
@@ -0,0 +1,29 @@
+package got
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestRange(t *testing.T) {
+	t.Run("Range(0)", func(t *testing.T) {
+		r := Range(0)
+		if r != nil {
+			t.Errorf("Range(0) = %v; want nil", r)
+		}
+	})
+	t.Run("Range(1)", func(t *testing.T) {
+		r := Range(1)
+		e := []int{0}
+		if !slices.Equal(r, e) {
+			t.Errorf("Range(1) = %v; want %v", r, e)
+		}
+	})
+	t.Run("Range(5)", func(t *testing.T) {
+		r := Range(5)
+		e := []int{0, 1, 2, 3, 4}
+		if !slices.Equal(r, e) {
+			t.Errorf("Range(5) = %v; want %v", r, e)
+		}
+	})
+}
