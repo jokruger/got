d0904530ce39dd15ff6946597f6e85ea7fe6ac87
Yurii Biurher <yurii.biurher@zorallabs.com>
2024-12-17 15:21:40 +0200

    FilterInPlace

--- slices/filter.go
+++ slices/filter.go
@@ -13,6 +13,18 @@ func Filter[T any](s []T, f func(T) bool) []T {
 	return res
 }
 
+// FilterInPlace removes all elements from slice 's' that do not satisfy the predicate f and returns the modified slice.
+func FilterInPlace[T any](s []T, f func(T) bool) []T {
+	i := 0
+	for _, v := range s {
+		if f(v) {
+			s[i] = v
+			i++
+		}
+	}
+	return s[:i]
+}
+
 // FilterToSeq returns a new sequence containing only the elements of slice 's' that satisfy the predicate f.
 func FilterToSeq[T any](s []T, f func(T) bool) iter.Seq[T] {
 	return func(yield func(T) bool) {
