18c984d9945ff31fca9dd5ca9a5b63686068cd0c
Yurii Biurher <yurii.biurher@zorallabs.com>
2024-12-17 15:24:04 +0200

    FilterInPlace tests

--- filter_test.go
+++ filter_test.go
@@ -31,6 +31,17 @@ func TestFilter(t *testing.T) {
 		}
 	})
 
+	t.Run("ints slice in place", func(t *testing.T) {
+		s := []int{1, 2, 3, 4, 5}
+		s = gslices.FilterInPlace(s, func(i int) bool { return i%2 == 0 })
+		if len(s) != 2 {
+			t.Errorf("Expected 2, got %d", len(s))
+		}
+		if s[0] != 2 || s[1] != 4 {
+			t.Errorf("Expected [2, 4], got %v", s)
+		}
+	})
+
 	t.Run("ints iter", func(t *testing.T) {
 		s := slices.Values([]int{1, 2, 3, 4, 5})
 		r := make([]int, 0)
