3204ccf7ce392b25b7ed9f135f7d51e6fd7ecdcc
Yurii Biurher <yurii.biurher@zorallabs.com>
2025-08-01 12:48:21 +0300

    refactoring

--- sliceutil/minmax.go
+++ sliceutil/minmax.go
@@ -2,11 +2,33 @@ package sliceutil
 
 import "cmp"
 
-// MinMax calculates the minimum and maximum values of a slice based on a given function.
-func MinMax[T any, K cmp.Ordered](s []T, f func(T) K) (K, K, bool) {
+// MinMax finds minimum and maximum elements in a slice.
+func MinMax[T any](s []T, compare func(T, T) int) (T, T) {
+	if len(s) == 0 {
+		var zero T
+		return zero, zero
+	}
+
+	min := s[0]
+	max := min
+
+	for _, v := range s[1:] {
+		if compare(v, min) < 0 {
+			min = v
+		}
+		if compare(v, max) > 0 {
+			max = v
+		}
+	}
+
+	return min, max
+}
+
+// MapMinMax applies a function to each element of the slice and finds the minimum and maximum values based on the results of that function.
+func MapMinMax[T any, K cmp.Ordered](s []T, f func(T) K) (K, K) {
 	if len(s) == 0 {
 		var zero K
-		return zero, zero, false
+		return zero, zero
 	}
 
 	min := f(s[0])
@@ -22,5 +44,5 @@ func MinMax[T any, K cmp.Ordered](s []T, f func(T) K) (K, K, bool) {
 		}
 	}
 
-	return min, max, true
+	return min, max
 }
--- tests/minmax_test.go
+++ tests/minmax_test.go
@@ -9,14 +9,13 @@ import (
 func TestMinMax(t *testing.T) {
 	t.Run("ints slice", func(t *testing.T) {
 		s := []int{1, 2, 3, 4, 5}
-		min, max, ok := sliceutil.MinMax(s, func(x int) int { return x })
-		if min != 1 || max != 5 || ok != true {
-			t.Errorf("expected (1, 5, true) got (%v, %v, %v)", min, max, ok)
+		min, max := sliceutil.MinMax(s, func(a, b int) int { return a - b })
+		if min != 1 || max != 5 {
+			t.Errorf("expected (1, 5) got (%v, %v)", min, max)
 		}
-
-		min, max, ok = sliceutil.MinMax(nil, func(x int) int { return x })
-		if min != 0 || max != 0 || ok != false {
-			t.Errorf("expected (0, 0, false) got (%v, %v, %v)", min, max, ok)
+		min, max = sliceutil.MapMinMax(s, func(x int) int { return x })
+		if min != 1 || max != 5 {
+			t.Errorf("expected (1, 5) got (%v, %v)", min, max)
 		}
 	})
 }
